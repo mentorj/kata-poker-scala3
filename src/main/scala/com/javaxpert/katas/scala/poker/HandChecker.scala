b1b0edf5c5fefa00ebcc1637a5a864042369ce70
deadbrain <jerome@javaxpert.com>
2024-05-11 16:53:09 +0200

    step 7 ter - a single HoF

--- src/main/scala/com/javaxpert/katas/scala/poker/HandChecker.scala
+++ src/main/scala/com/javaxpert/katas/scala/poker/HandChecker.scala
@@ -4,44 +4,42 @@ package com.javaxpert.katas.scala.poker
 object HandChecker {
 
 
-  def handConformsToCriteria(hand:Hand)(desiredInstances:Int)(f:Card => Selectable)(patternForSelector:Int) : Boolean =
-    hand.cards.groupBy((c) => f.apply(c))
+  def handConformsToCriteria(hand: Hand)(desiredInstances: Int)(f: Card => Selectable)(patternForSelector: Int)(cardsPostFilterPredicate: (List[Card]) => Boolean): Boolean =
+    hand.cards
+      .sortWith(sortByCardRank)
+      .groupBy((c) => f.apply(c))
       .filter((rank, list) => list.size == patternForSelector)
-      .size==desiredInstances
+      .filter((rank, list) => cardsPostFilterPredicate.apply(list))
+      .size == desiredInstances
 
   def containsPair(hand: Hand): Boolean =
-      handConformsToCriteria(hand)(1)(_.rank)(2)
+    handConformsToCriteria(hand)(1)(_.rank)(2)(_ => true)
 
-  def contains2Pairs(hand: Hand):Boolean=
-    handConformsToCriteria(hand)(2)(_.rank)(2)
+  def contains2Pairs(hand: Hand): Boolean =
+    handConformsToCriteria(hand)(2)(_.rank)(2)(_ => true)
 
-  def handContainsBrelan(hand: Hand):Boolean=
-    handConformsToCriteria(hand)(1)(_.rank)(3)
+  def handContainsBrelan(hand: Hand): Boolean =
+    handConformsToCriteria(hand)(1)(_.rank)(3)(_ => true)
 
   def sortByCardRank(c1: Card, c2: Card) =
     c1.rank.ordinal > c2.rank.ordinal
 
-  def handContainsColor(hand: Hand):Boolean=
-    handConformsToCriteria(hand)(1)(_.color)(5)
+  def handContainsColor(hand: Hand): Boolean =
+    handConformsToCriteria(hand)(1)(_.color)(5)(_ => true)
 
 
-def hansIsAFull(hand:Hand):Boolean={
-  handContainsBrelan(hand) && containsPair(hand)
-}
+  def hansIsAFull(hand: Hand): Boolean = {
+    handContainsBrelan(hand) && containsPair(hand)
+  }
 
-def handContainsSquare(hand:Hand):Boolean=
-  handConformsToCriteria(hand)(1)(_.rank)(4)
+  def handContainsSquare(hand: Hand): Boolean = {
+    handConformsToCriteria(hand)(1)(_.rank)(4)(_ => true)
+  }
 
-def handContainsQuinte(hand:Hand):Boolean=
-  hand.cards
-    .sortWith(sortByCardRank)
-    .groupBy(_.color)
-    .filter((rank, list) => list.size == 5)
-    .filter((c:Color,cards:List[Card]) =>
-       cards(0).rank.ordinal- cards(4).rank.ordinal ==  4)
-    .isEmpty == false
-
-
+  def handContainsQuinte(hand: Hand): Boolean = {
+    val pred = (cards: List[Card]) => cards.apply(0).rank.ordinal - cards.apply(4).rank.ordinal == 4
+    handConformsToCriteria(hand)(1)(_.color)(5)(pred)
+  }
 
 
 }
